832a6d9de4508998dd17b3bcf583cdad6114e39a
Srilatha <177000222+srilatha940@users.noreply.github.com>
2026-01-27 12:23:33 +0530

    commit

--- backend/gym/gym/settings.py
+++ backend/gym/gym/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tracker',
-      'corsheaders',
-      'User',
+    'corsheaders',
+    'User',
 ]
 
 MIDDLEWARE = [
